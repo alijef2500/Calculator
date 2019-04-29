4f05a7ef77afa1954d37f01bfe143824b7c5d0b2
kheder yazgi <33778071+khederyy1997@users.noreply.github.com>
2019-04-29 11:28:58 -0700

    Update App.js
    navigation bar to moving between

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Button
+} from "reactstrap";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">
+            Home
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Router>
+              <div>
+                <Header />
+
+                <Route path="/convertor" component={Convertor} />
+                <Route path="/calculator" component={Calculator} />
+              </div>
+            </Router>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+class Convertor extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Convertor</h2>
+      </div>
+    );
+  }
+}
+
+function Calculator() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Calculator</h2>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <Nav navbar>
+      <NavItem>
+        <Link to="/calculator">Calculator</Link>
+      </NavItem>
+      <NavItem>
+        <Link to="/convertor">Convertor</Link>
+      </NavItem>
+    </Nav>
   );
 }
 
